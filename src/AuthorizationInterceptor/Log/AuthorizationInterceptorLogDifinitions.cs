4e164422a7b66d1bcf364cae205f0966ced1a8e9
Luiz Adolfo <luiz-adolfo@hotmail.com>
2025-03-17 11:31:58 -0300

    Created LogDinifitions

---
+++ src/AuthorizationInterceptor/Log/AuthorizationInterceptorLogDifinitions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AuthorizationInterceptor.Log;
+
+public static partial class AuthorizationInterceptorLogDifinitions
+{
+    [LoggerMessage(Level = LogLevel.Warning, Message = "No interceptor was configured for HttpClient `{httpClientName}`. A Runtime interceptor was used instead. It is recommended to use at least the MemoryCache interceptor.")]
+    public static partial void LogNoInterceptorUsed(this ILogger logger, string httpClientName);
+}
+
